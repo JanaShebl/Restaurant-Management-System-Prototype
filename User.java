5d29fe437b727fc6e51794d987bc5c56b6d9d458
JanaShebl <janamohamed195200@gmail.com>
2025-11-30 09:38:55 +0200

    Update User.java

--- User.java
+++ User.java
@@ -3,11 +3,39 @@ public abstract class User {
     protected String username;
     protected String password;
     protected String email;
+    protected String phone;
     
+    // Constructor
+    public User(String userId, String username, String password, String email, String phone) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+    }
+    
+    // Abstract methods
     public abstract boolean login(String username, String password);
     public abstract void logout();
-    public abstract boolean updateInfo();
-    public boolean changePassword(String oldPass, String newPass) {
-        // implementation
+    
+    // Concrete methods
+    public boolean updateProfile(String email, String phone) {
+        this.email = email;
+        this.phone = phone;
+        return true;
     }
+    
+    public boolean changePassword(String oldPassword, String newPassword) {
+        if (this.password.equals(oldPassword)) {
+            this.password = newPassword;
+            return true;
+        }
+        return false;
+    }
+    
+    // Getters and Setters
+    public String getUserId() { return userId; }
+    public String getUsername() { return username; }
+    public String getEmail() { return email; }
+    public String getPhone() { return phone; }
 }
