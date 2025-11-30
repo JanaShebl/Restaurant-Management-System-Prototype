bc402ce765726f4a5e40e1b6784f32e75dbb5b4e
JanaShebl <janamohamed195200@gmail.com>
2025-11-30 22:45:53 +0200

    Refactor User class to use int for userId
    Updated userId from String to int and modified constructor and methods accordingly.

--- User.java
+++ User.java
@@ -1,17 +1,13 @@
 public abstract class User {
-    protected String userId;
+    protected int userId;
     protected String username;
     protected String password;
-    protected String email;
-    protected String phone;
     
     // Constructor
-    public User(String userId, String username, String password, String email, String phone) {
+    public User(int userId, String username, String password) {
         this.userId = userId;
         this.username = username;
         this.password = password;
-        this.email = email;
-        this.phone = phone;
     }
     
     // Abstract methods
@@ -19,23 +15,21 @@ public abstract class User {
     public abstract void logout();
     
     // Concrete methods
-    public boolean updateProfile(String email, String phone) {
-        this.email = email;
-        this.phone = phone;
-        return true;
+        public void updateInfo(String username, String password) {
+        this.username = username;
+        this.password = password;
+ 
     }
     
-    public boolean changePassword(String oldPassword, String newPassword) {
-        if (this.password.equals(oldPassword)) {
-            this.password = newPassword;
-            return true;
-        }
-        return false;
-    }
+    // public boolean changePassword(String oldPassword, String newPassword) {
+    //     if (this.password.equals(oldPassword)) {
+    //         this.password = newPassword;
+    //         return true;
+    //     }
+    //     return false;
+    // }
     
     // Getters and Setters
-    public String getUserId() { return userId; }
+    public int getUserId() { return userId; }
     public String getUsername() { return username; }
-    public String getEmail() { return email; }
-    public String getPhone() { return phone; }
 }
